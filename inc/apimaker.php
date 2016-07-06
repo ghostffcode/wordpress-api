b498fb63bd913483efa6651882721a14522ce5cf
PoeticBliss <meremezeukah@gmail.com>
2016-07-06 00:12:04 -0400

    initial commit

---
+++ inc/apimaker.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This is the class that gets data from database and transforms to API data
+ */
+class apimaker {
+
+  private server = 'localhost',
+          db = 'wordpress',
+          table = 'wp_posts',
+          db_user = 'root'
+
+  function __construct(argument)
+  {
+    # code...
+  }
+}
+
+
+
+ ?>
